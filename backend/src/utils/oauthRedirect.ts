619b180aa79ba270ed2434b4900ae2fecb1174bb
Dott-Media-Git <kutesaug1@gmail.com>
2026-06-24 16:29:56 +0300

    Correct social OAuth authorization and callback paths

---
+++ backend/src/utils/oauthRedirect.ts
@@ -0,0 +1,8 @@
+const frontendBaseUrl = () =>
+  (process.env.FRONTEND_URL || process.env.PUBLIC_APP_URL || 'https://dottmediaapk.web.app').replace(/\/+$/, '');
+
+export const oauthSuccessRedirect = (platform: string) => {
+  const url = new URL(`${frontendBaseUrl()}/integrations`);
+  url.searchParams.set('connected', platform);
+  return url.toString();
+};
