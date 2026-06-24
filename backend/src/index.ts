ac87f694ad6f43095acbdfc20eeb301e712b400c
Dott-Media-Git <kutesaug1@gmail.com>
2026-06-24 15:49:25 +0300

    Use reliable same-tab social OAuth redirects

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -113,6 +113,7 @@ app.use(
   }),
 );
 app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: false, limit: '64kb' }));
 app.use('/flutterwave/webhook', flutterwaveRoutes);
 app.use(
   morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms :req[x-request-id]'),
